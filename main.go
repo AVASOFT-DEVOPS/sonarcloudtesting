8d1b733695591dbf5443eb2c0e6433e6eb3eb14f
SubhasiniBabu <121109357+SubhasiniBabu@users.noreply.github.com>
2023-03-31 00:43:55 +0530

    Updated main file with validation & testing logic

--- main.go
+++ main.go
@@ -12,33 +12,46 @@ import (
 
 func main() {
 	// Create a new router instance
-	r := mux.NewRouter()
+	r := CreateRouter()
 
 	// Define the "/hello/{name}" endpoint
-	r.HandleFunc("/hello/{name}", func(w http.ResponseWriter, r *http.Request) {
-		// Extract the "name" path parameter from the request
-		vars := mux.Vars(r)
-		name := vars["name"]
-
-		validate := validate(name)
-
-		if validate != nil {
-			fmt.Fprintf(w, validate.Message)
-			return
-		}
-
-		// Write the response back to the client
-		fmt.Fprintf(w, "Hi, %s! How you doing?", name)
-	}).Methods("GET")
+	r.HandleFunc("/hello/{name}", helloHandler).Methods("GET")
 
 	// Start the HTTP server
 	log.Printf("Listening on port 8080")
 	log.Fatal(http.ListenAndServe(":8080", r))
 }
 
+func CreateRouter() *mux.Router {
+	// Create a new router instance
+	r := mux.NewRouter()
+
+	// Define the "/hello/{name}" endpoint
+	r.HandleFunc("/hello/{name}", helloHandler).Methods("GET")
+
+	return r
+}
+
 type ValidateResponse struct {
 	Code    string
 	Message string
+}
+
+func helloHandler(w http.ResponseWriter, r *http.Request) {
+	// Extract the "name" path parameter from the request
+	vars := mux.Vars(r)
+	name := vars["name"]
+
+	validationResult := validate(name)
+
+	if validationResult != nil {
+		// If validation fails, return an HTTP 400 Bad Request error
+		http.Error(w, validationResult.Message, http.StatusBadRequest)
+		return
+	}
+
+	// Write the response back to the client
+	fmt.Fprintf(w, "Hi, %s! How you doing?", name)
 }
 
 func validate(req string) *ValidateResponse {
